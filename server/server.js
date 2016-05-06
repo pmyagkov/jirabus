8db9791e3d0bf22a2905eac9637db2072ac2eb25
Pavel Myagkov <p.myagkov@corp.mail.ru>
2016-05-06 15:49:36 +0300

    Add server logic to send feedback to telegram

--- server/server.js
+++ server/server.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var app = express();
+var request = require('request');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.post('/feedback', function (req, res) {
+  var text = req.body.text + '\n\n' + req.body.name;
+  var url = 'https://api.telegram.org/bot190843896:AAFchCFzLnhq-H9FG0wZABviItMBA3_HCuo/sendMessage';
+  var data = {chat_id: '@JIRAbusFeedback', text: text};
+
+  request.post({ url: url, form: data }, function (err, httpResponse, body) {
+    console.log('BODY', body);
+
+    var text;
+    if (err || !body.ok) {
+      text = JSON.stringify(err) + '\n\n' + JSON.stringify(body);
+      res.status(502).send(text);
+    } else {
+      res.status(200).end();
+    }
+
+  });
 });
 
 app.listen(8000, function () {
